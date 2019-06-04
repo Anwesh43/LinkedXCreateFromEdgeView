5d8effe3088927db850edb8327a59d3b7ec85a7e
Anwesh43 <anweshthecool0@gmail.com>
2019-06-04 09:39:21 +0530

    adding delay as constant

--- xcreateedgeview/src/main/java/com/anwesh/uiprojects/xcreateedgeview/XCreateEdgeView.kt
+++ xcreateedgeview/src/main/java/com/anwesh/uiprojects/xcreateedgeview/XCreateEdgeView.kt
@@ -22,6 +22,7 @@ val sizeFactor : Float = 2.9f
 val foreColor : Int = Color.parseColor("#0D47A1")
 val backColor : Int = Color.parseColor("#BDBDBD")
 val rotDeg : Float = 45f
+val delay : Long = 20
 
 fun Int.inverse() : Float = 1f / this
 fun Float.scaleFactor() : Float = Math.floor(this / scDiv).toFloat()
@@ -110,7 +111,7 @@ class XCreateEdgeView(ctx : Context) : View(ctx) {
             if (animated) {
                 cb()
                 try {
-                    Thread.sleep(50)
+                    Thread.sleep(delay)
                     view.invalidate()
                 } catch(ex : Exception) {
 
