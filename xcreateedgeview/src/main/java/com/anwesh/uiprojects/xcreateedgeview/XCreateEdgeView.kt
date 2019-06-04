641cdfb451d79ef3e1e7192d9e20b36ddcf8c483
Anwesh43 <anweshthecool0@gmail.com>
2019-06-04 08:22:58 +0530

    adding constants and libararies

--- xcreateedgeview/src/main/java/com/anwesh/uiprojects/xcreateedgeview/XCreateEdgeView.kt
+++ xcreateedgeview/src/main/java/com/anwesh/uiprojects/xcreateedgeview/XCreateEdgeView.kt
@@ -3,3 +3,21 @@ package com.anwesh.uiprojects.xcreateedgeview
 /**
  * Created by anweshmishra on 04/06/19.
  */
+
+import android.view.View
+import android.view.MotionEvent
+import android.app.Activity
+import android.content.Context
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.Color
+
+val nodes : Int = 5
+val lines : Int = 2
+val parts : Int = 2
+val scGap : Float = 0.05f
+val scDiv : Double = 0.51
+val strokeFactor : Int = 90
+val sizeFactor : Float = 2.9f
+val foreColor : Int = Color.parseColor("#0D47A1")
+val backColor : Int = Color.parseColor("#BDBDBD")
