300c945812c1d803672cc8952e84e2d6a5649369
Anwesh43 <anweshthecool0@gmail.com>
2019-06-04 09:00:49 +0530

    adding view in activity

--- app/src/main/java/com/anwesh/uiprojects/linkedxcreatefromedgeview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedxcreatefromedgeview/MainActivity.kt
@@ -2,11 +2,12 @@ package com.anwesh.uiprojects.linkedxcreatefromedgeview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.anwesh.uiprojects.xcreateedgeview.XCreateEdgeView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        XCreateEdgeView.create(this)
     }
 }
