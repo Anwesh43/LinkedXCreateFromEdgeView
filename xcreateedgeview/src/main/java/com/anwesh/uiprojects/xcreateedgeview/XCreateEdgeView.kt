a466562320467e1bc62cf3d3ffc6d47825401857
Anwesh43 <anweshthecool0@gmail.com>
2019-06-04 08:51:58 +0530

    adding XCENode class

--- xcreateedgeview/src/main/java/com/anwesh/uiprojects/xcreateedgeview/XCreateEdgeView.kt
+++ xcreateedgeview/src/main/java/com/anwesh/uiprojects/xcreateedgeview/XCreateEdgeView.kt
@@ -126,4 +126,48 @@ class XCreateEdgeView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class XCENode(var i : Int, val state : State = State()) {
+
+        private var next : XCENode ?= null
+        private var prev : XCENode ?= null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < nodes - 1) {
+                next = XCENode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawXCENode(i, state.scale, paint)
+            next?.draw(canvas, paint)
+        }
+
+        fun update(cb : (Int, Float) -> Unit) {
+            state.update {
+                cb(i, it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : XCENode {
+            var curr : XCENode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
