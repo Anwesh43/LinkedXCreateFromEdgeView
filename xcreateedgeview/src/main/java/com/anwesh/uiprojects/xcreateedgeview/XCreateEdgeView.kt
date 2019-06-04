0c0c2294ea315c5515e52a7092790cdbbcf00f80
Anwesh43 <anweshthecool0@gmail.com>
2019-06-04 08:54:15 +0530

    adding XCreateEdge class

--- xcreateedgeview/src/main/java/com/anwesh/uiprojects/xcreateedgeview/XCreateEdgeView.kt
+++ xcreateedgeview/src/main/java/com/anwesh/uiprojects/xcreateedgeview/XCreateEdgeView.kt
@@ -170,4 +170,28 @@ class XCreateEdgeView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class XCreateEdge(var i : Int) {
+
+        private val root : XCENode = XCENode(0)
+        private var curr : XCENode = root
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            root.draw(canvas, paint)
+        }
+
+        fun update(cb : (Int, Float) -> Unit) {
+            curr.update {i, scl ->
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(i, scl)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
